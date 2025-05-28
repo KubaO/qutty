c05547cc48543719e83f10711e0bb8b5184cee25
Kuba Sunderland-Ober <kuba@mareimbrium.org>
2025-05-28 14:56:14 +0200

    Update the PuTTY sources and get it to build

--- puttysrc/windows/windefs.c
+++ puttysrc/windows/windefs.c
@@ -17,11 +17,7 @@ FontSpec *platform_default_fontspec(const char *name)
 Filename *platform_default_filename(const char *name)
 {
     if (!strcmp(name, "LogFileName"))
-#ifdef IS_QUTTY
-        return filename_from_str("qutty.log");
-#else
         return filename_from_str("putty.log");
-#endif
     else
         return filename_from_str("");
 }
